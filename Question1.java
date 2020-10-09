9dcd0d93facd1b08842a3c80d8b0889e3ebb89ba
NeazMorshed1971 <45413763+NeazMorshed1971@users.noreply.github.com>
2020-10-09 23:04:25 +0600

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -12,10 +12,10 @@ public class Question1 {
 
 	
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
-	      // Register driver and create driver instance
+	      
 		   
 	      Class.forName(driverName);
-	      // get connection
+	      
 	      
 	       con = DriverManager.getConnection("jdbc:hive2://localhost:10000/sh", "", "");
 	       stmt = con.createStatement();
